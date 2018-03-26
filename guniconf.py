29786a878b1c170c62a2c008637725fab2c3c2c9
Og Astorga <og@ogastorga.com>
2018-03-26 11:21:30 -0600

    Run service as root

--- guniconf.py
+++ guniconf.py
@@ -1,7 +1,7 @@
 pidfile        = '/run/gunicorn/walletbot.pid'
 bind           = ['0.0.0.0:5000']
-user           = 'www-data'
-group          = 'www-data'
+user           = 'root'
+group          = 'root'
 accesslog      = '/var/log/gunicorn/walletbot.access.log'
 errorlog       = '/var/log/gunicorn/walletbot.error.log'
 raw_env        = ['WALLETBOT_SETTINGS=/root/apps/walletbot/settings_production.py']
