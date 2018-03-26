21e08954dfddc8c7ed3e9ec626fc44ce8ee0d457
Og Astorga <og@ogastorga.com>
2018-03-25 18:13:44 -0600

    Telegram webhook endpoint

--- walletbot/__init__.py
+++ walletbot/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 
 
 # Read config variables
@@ -10,4 +11,22 @@ app.config.from_envvar('WALLETBOT_SETTINGS', silent=True)
 def hello():
     return jsonify({
         'message': 'Hello, my name is walletbot'
+    })
+
+@app.route('/webhook/<token>', methods=['POST'])
+def telegram_webhook(token):
+    if token != app.config['TELEGRAM_AUTH_TOKEN']:
+        return jsonify({
+            'ok': False,
+            'error_code': 400,
+            'description': 'Bad Request: wrong telegram auth token',
+        })
+
+    raw_body = request.get_data()
+    update = json.loads(raw_body)
+
+    print(update)
+
+    return jsonify({
+        'message': 'ok'
     })
